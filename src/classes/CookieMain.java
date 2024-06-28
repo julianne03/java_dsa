48aa253d4f4d5f67c3eaa310577a398bdd529b58
julianne03 <julianneyi03@gmail.com>
2024-06-27 21:57:58 -0700

    classes

---
+++ src/classes/CookieMain.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class CookieMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cookie cookieOne = new Cookie("green");
+		System.out.println(cookieOne.getColor());
+	}
+
+}
