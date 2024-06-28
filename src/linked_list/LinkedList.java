bdc11368d403cccd05defa1b37b72ba76267e360
julianne03 <julianneyi03@gmail.com>
2024-06-28 11:12:17 -0700

    LinkedList - append

---
+++ src/linked_list/LinkedList.java
@@ -0,0 +1,59 @@
+package linked_list;
+
+public class LinkedList {
+	private Node head;
+	private Node tail;
+	private int length;
+	
+	
+	class Node {
+		int value;
+		Node next;
+		
+		Node(int value) {
+			this.value = value;
+		}
+	}
+	
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		this.head = newNode;
+		this.tail = newNode;
+		this.length = 1;
+	}
+	
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
+	
+	public void append(int value) {
+		Node newNode = new Node(value);
+		
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		length++;
+	}
+
+	public void getHead() {
+		System.out.println("Head: " + head.value);
+	}
+
+	public void getTail() {
+		System.out.println("Tail: " + tail.value);
+	}
+
+	public void getLength() {
+		System.out.println("Length: " + length);
+	}
+	
+	
+}
---
+++ src/linked_list/Main.java
@@ -0,0 +1,17 @@
+package linked_list;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList myLinkedList = new LinkedList(4);
+		myLinkedList.append(7);
+		
+		myLinkedList.getHead();
+		myLinkedList.getTail();
+		myLinkedList.getLength();
+		
+		myLinkedList.printList();
+	}
+
+}
