a8b58757e24da3360d0bf8b5cf0e85bee8abb04f
julianne03 <julianneyi03@gmail.com>
2024-06-29 14:11:13 -0700

    LinkedList - get method

--- src/linked_list/Main.java
+++ src/linked_list/Main.java
@@ -4,19 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LinkedList myLinkedList = new LinkedList(4);
-		myLinkedList.append(7);
-		myLinkedList.append(42);
-		myLinkedList.append(11);
+		LinkedList myLinkedList = new LinkedList(0);
+		myLinkedList.append(1);
+		myLinkedList.append(2);
+		myLinkedList.append(3);
 		
 		myLinkedList.printList();
+		System.out.println();
 		
-		System.out.println("Node removed: " + myLinkedList.removeLast().value);
-		System.out.println("Node removed: " + myLinkedList.removeLast().value);
-		
-		myLinkedList.prepend(9);
-		System.out.println("Node removed: " + myLinkedList.removeFirst().value);
-		System.out.println("Node removed: " + myLinkedList.removeFirst().value);
+		System.out.println(myLinkedList.get(1).value);
+//		
+//		System.out.println("Node removed: " + myLinkedList.removeLast().value);
+//		System.out.println("Node removed: " + myLinkedList.removeLast().value);
+//		
+//		myLinkedList.prepend(9);
+//		System.out.println("Node removed: " + myLinkedList.removeFirst().value);
+//		System.out.println("Node removed: " + myLinkedList.removeFirst().value);
 
 		
 		myLinkedList.printList();
